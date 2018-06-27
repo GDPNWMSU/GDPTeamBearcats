c257a3f457e2750b6471a65d4b7e68ef4ae72442
Raja Srikar Karthik Chinta <S530460@nwmissouri.edu>
2018-06-27 14:31:45 -0500

    Add code to include Bootstrap libraries in Header.php

---
+++ src/Header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Header file.
+ */
+use PhpOffice\PhpSpreadsheet\Helper\Sample;
+
+error_reporting(E_ALL);
+
+?>
+<html>
+<head>
+    <title>Reporter Parse Excel sheets</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="./libraries/PhpSpreadsheet-develop/samples/bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./libraries/PhpSpreadsheet-develop/samples/bootstrap/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="./libraries/PhpSpreadsheet-develop/samples/bootstrap/css/phpspreadsheet.css"/>
+    <script src="./libraries/PhpSpreadsheet-develop/samples/bootstrap/js/jquery.min.js"></script>
+    <script src="./libraries/PhpSpreadsheet-develop/samples/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
