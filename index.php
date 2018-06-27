cbd7b5cda03187f6bdd98ff1c01347021e229cc1
Raja Srikar Karthik Chinta <S530460@nwmissouri.edu>
2018-06-27 14:17:29 -0500

    Add Form to import new Excel sheet

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 	window.location.href="src/"
-</script>
+</script> -->
+<form action="./src/file_importer.php" method="post" enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="Filename" id="Filename"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
