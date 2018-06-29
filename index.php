36d69a2c71a322d1ea47d5156964f2948135bb6c
Raja Srikar Karthik Chinta <S530460@nwmissouri.edu>
2018-06-28 21:54:26 -0500

    Add datatables and pre-loader to index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<!-- <script type="text/javascript">
-	window.location.href="src/"
-</script> -->
-<form action="./src/file_importer.php" method="post" enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="Filename" id="Filename"><br>
-<input type="submit" name="submit" value="Submit">
-</form>
