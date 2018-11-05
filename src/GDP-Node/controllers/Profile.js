d97f579527ef1b7cf163277cf7ab381c184e294d
PRAVALIKA KAWALI <S530936@nwmissouri.edu>
2018-11-04 21:09:02 -0600

    Add validations for Profile.js

--- src/GDP-Node/controllers/Profile.js
+++ src/GDP-Node/controllers/Profile.js
@@ -1,36 +1,41 @@
 var express = require('express')
 var mysql = require("mysql");
-var logger=require('morgan')
+var logger = require('morgan')
 var engines = require('consolidate')
 var connection = require('../config/db_connection');
 var bodyParser = require('body-parser');
-const router = express.Router()
+const router = express.Router();
+const crypto = require('crypto');
 
 
 let message = ''
 router.get('/', (req, res, next) => {
     res.render('profile.ejs', {
         title: 'profile',
-        message: message
+        message: message,
+        username: req.session.user.email,
+        firstName: req.session.user.firstName,
+        user: req.session.user
     })
 })
-router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/add_profile', function(request, response){
-    console.log(request.body);      
-        var sql = `INSERT INTO profile VALUES ('${request.body.first_name}','${request.body.last_name}','${request.body.mobile}','${request.body.email}','${request.body.password}','${request.body.password2}');`
-        connection.query(sql, function (err, result) {
-          if (err){
-           message = false
-           response.redirect('/profile/')
+router.post('/add_profile', function (request, response) {
+    console.log(request.body);
+    let cryptoPwd = crypto.createHash('sha1').update('' + request.body.password).digest('hex');
+    //var sql = `INSERT INTO profile VALUES ('${request.body.first_name}','${request.body.last_name}','${request.body.mobile}','${request.body.email}','${request.body.password}','${request.body.password2}');`
+    connection.query("update tbl_users SET email=?,password=?, firstName=?, lastName=? where id=?", [request.body.email, cryptoPwd, request.body.first_name, request.body.last_name, request.session.user.id], function (err, result) {
+        if (err) {
+            message = false
+            response.redirect('/profile/')
             throw err;
-          } 
-          
-          console.log("1 record inserted");
-          message = true
-          response.redirect('/profile/')
-        });
-        
-  });
+        }
+
+        console.log("1 record inserted");
+        message = true
+        response.redirect('/profile/')
+    });
+
+});
 
 module.exports = router
